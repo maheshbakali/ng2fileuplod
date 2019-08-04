158b54eaba7a2a5dfbef839b213835c2455c62ec
Mahesh Bakali <mahesh.bakali@gmail.com>
2019-08-04 23:31:01 +1000

    csharp api added

---
+++ FileUploadController.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch.Operations;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileUploadApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[EnableCors]
+    public class FileUploadController : ControllerBase
+    {
+        //[HttpPost, DisableRequestSizeLimit]
+        //public IActionResult Upload()
+        //{
+        //    try
+        //    {
+        //        var files = Request.Form.Files;
+        //        var folderName = Path.Combine("Resources", "Images");
+        //        var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+        //        foreach(var file in files)
+        //        {
+        //            if (file.Length > 0)
+        //            {
+        //                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+        //                var fullPath = Path.Combine(pathToSave, fileName);
+        //                var dbPath = Path.Combine(folderName, fileName);
+
+        //                using (var stream = new FileStream(fullPath, FileMode.Create))
+        //                {
+        //                    file.CopyTo(stream);
+        //                }                        
+        //            }
+        //            else
+        //            {
+        //                return BadRequest();
+        //            }
+        //        }                
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(500, "Internal server error");
+        //    }
+
+        //    return Ok();
+        //}
+
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(IFormFile file)
+        {
+            long size = file.Length;
+
+            // full path to file in temp location
+            var filePath = @"C:\Users\Mahesh Bakali\Downloads\Drop";
+
+            var folderName = Path.Combine("Resources", "Images");
+            var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+            var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+            var fullPath = Path.Combine(pathToSave, fileName);
+            filePath = Path.Combine(folderName, fileName);
+
+            //foreach (var file in files)
+            //{
+            if (file.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+            //}
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = 1, size, filePath });
+        }
+
+    }
+
+    public class FileUploadOperation : IOperationFilter
+    {
+        public void Apply(Swashbuckle.AspNetCore.Swagger.Operation operation, OperationFilterContext context)
+        {
+            if (operation.OperationId.ToLower() == "apivaluesuploadpost")
+            {
+                operation.Parameters.Clear();
+                operation.Parameters.Add(new NonBodyParameter
+                {
+                    Name = "uploadedFile",
+                    In = "formData",
+                    Description = "Upload File",
+                    Required = true,
+                    Type = "file"
+                });
+                operation.Consumes.Add("multipart/form-data");
+            }
+        }
+    }
+
+    /*
+     using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FileUploadApi.Controllers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace FileUploadApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("MyPolicy",
+                builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info() { Title = "My API", Version = "v1" });
+            });
+
+            services.AddSwaggerGen();
+            services.ConfigureSwaggerGen(options =>
+            {
+                //options.SingleApiVersion(new Info
+                //{
+                //    Version = "v1",
+                //    Title = "My API",
+                //    Description = "My First Core Web API",
+                //    TermsOfService = "None",
+                //    Contact = new Contact() { Name = "Talking Dotnet", Email = "contact@talkingdotnet.com", Url = "www.talkingdotnet.com" }
+                //});
+                //options.IncludeXmlComments(GetXmlCommentsPath());
+                options.DescribeAllEnumsAsStrings();
+                options.OperationFilter<FileUploadOperation>(); //Register File Upload Operation Filter
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors("MyPolicy");
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
+
+     */
+}
